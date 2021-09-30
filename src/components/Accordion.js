1443583dce9233fd2f1a0d58d0edb94850a082da
atesfay <abrahamkt@yahoo.com>
2021-09-29 17:37:01 -0700

    Clean the Widget App and Add Accordion

---
+++ src/components/Accordion.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    console.log("Title Clicked", index);
+    setActiveIndex(index);
+  };
+
+  const renderedItem = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
+
+    return (
+      <React.Fragment key={item.title}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return <div className="ui styled accordion">{renderedItem}</div>;
+};
+
+export default Accordion;
